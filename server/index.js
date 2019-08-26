eb5bafb78914e53bb21549b82074cd95b8461271
geryoung <geryoungway@gmail.com>
2019-08-26 20:14:30 +0800

    添加静态资源

--- server/index.js
+++ server/index.js
@@ -3,13 +3,19 @@ const userRoute = require('./routes/user');
 const logMiddleware = require('./middleware/logger');
 const app = express();
 const logger = require('./logger');
+const path = require('path');
 
 function start() {
+    //config test
     require('../config');
     const timeout = require('config').get('timeout');
     logger.debug(timeout);
+
+    //设置
+    app.use(express.static(path.join(__dirname, 'public')))
+
     const port = 3000
-    app.get('/', (req, res) => res.send('Hello World!'))
+    app.get('/', (req, res) => res.render('index'))
     logMiddleware.addLogger(app);
     app.use('/user', userRoute);
     app.listen(port, () => console.log(`Example app listening on port ${port}!`))
